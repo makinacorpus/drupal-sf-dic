f6a80ee0b078fdbb125fe43c0709c744d0f653ba
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-01-12 11:16:27 +0100

    Added Drupal\Core\EntityManager passthrough with only the getStorage() method to fetch entity controllers

---
+++ lib/Drupal/Core/Entity/EntityManager.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Drupal\Core\Entity;
+
+/**
+ * Factory to fetch Drupal 7 entity controllers
+ */
+class EntityManager
+{
+    /**
+     * Get entity controller
+     *
+     * @return DrupalEntityControllerInterface
+     */
+    public function getStorage($entity_type)
+    {
+        return entity_get_controller($entity_type);
+    }
+}
