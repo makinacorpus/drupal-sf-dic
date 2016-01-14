367274e633a4701bb480db326b9503a072455fc9
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-01-14 16:46:34 +0100

    Added the event_dispatcher service and the associated compiler pass working on event_subscriber tags

--- sf_dic.container.php
+++ sf_dic.container.php
@@ -7,6 +7,7 @@ use Drupal\Core\DependencyInjection\ServiceProviderInterface;
 use MakinaCorpus\Drupal\Sf\Container\DependencyInjection\Compiler\VariablesCompilerPass;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\EventDispatcher\DependencyInjection\RegisterListenersPass;
 
 class ServiceProvider implements ServiceProviderInterface
 {
@@ -15,6 +16,8 @@ class ServiceProvider implements ServiceProviderInterface
      */
     public function register(ContainerBuilder $container)
     {
+        $container->addCompilerPass(new RegisterListenersPass('event_dispatcher', 'event_listener', 'event_subscriber'));
+
         // @todo This will make all variables read-only for the container
         //   which I'm not against but could cause some problems...
         // $container->addCompilerPass(new VariablesCompilerPass());
