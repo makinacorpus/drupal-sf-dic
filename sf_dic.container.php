2f950d7b9cdba95de31c216c38d928fc8febd22a
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-04-13 11:48:09 +0200

    added compiler pass to variable_set() all non already set container parameters

--- sf_dic.container.php
+++ sf_dic.container.php
@@ -4,13 +4,14 @@ namespace Drupal\Module\sf_dic;
 
 use Drupal\Core\DependencyInjection\ServiceProviderInterface;
 
+use MakinaCorpus\Drupal\Sf\Container\DependencyInjection\Compiler\AddConsoleCommandPass;
+use MakinaCorpus\Drupal\Sf\Container\DependencyInjection\Compiler\ParametersToVariablesPass;
+use MakinaCorpus\Drupal\Sf\Container\DependencyInjection\Compiler\TwigCompilerPass;
+
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\EventDispatcher\DependencyInjection\RegisterListenersPass;
-
-use MakinaCorpus\Drupal\Sf\Container\DependencyInjection\Compiler\AddConsoleCommandPass;
-use MakinaCorpus\Drupal\Sf\Container\DependencyInjection\Compiler\TwigCompilerPass;
 
 class ServiceProvider implements ServiceProviderInterface
 {
@@ -21,9 +22,8 @@ class ServiceProvider implements ServiceProviderInterface
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/Resources/config'));
 
-        $container->addCompilerPass(
-            new RegisterListenersPass('event_dispatcher', 'event_listener', 'event_subscriber')
-        );
+        $container->addCompilerPass(new RegisterListenersPass('event_dispatcher', 'event_listener', 'event_subscriber'));
+        $container->addCompilerPass(new ParametersToVariablesPass());
 
         if (class_exists('Symfony\\Component\\Console\\Command\\Command')) {
             $container->addCompilerPass(new AddConsoleCommandPass());
---
+++ src/Container/DependencyInjection/Compiler/ParametersToVariablesPass.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MakinaCorpus\Drupal\Sf\Container\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+class ParametersToVariablesPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        global $conf;
+
+        foreach ($container->getParameterBag()->all() as $key => $value) {
+            if (!array_key_exists($key, $conf)) {
+                variable_set($key, $value);
+            }
+        }
+    }
+}
