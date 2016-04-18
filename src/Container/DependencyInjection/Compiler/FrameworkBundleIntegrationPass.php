231d6ad54ca0cd7b29a4cc6e6838fa82aa50a384
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-04-18 11:09:16 +0200

    http_kernel definition was broken due to missing controller_resolver in non fullstack environements

--- src/Container/DependencyInjection/Compiler/FrameworkBundleIntegrationPass.php
+++ src/Container/DependencyInjection/Compiler/FrameworkBundleIntegrationPass.php
@@ -4,6 +4,7 @@ namespace MakinaCorpus\Drupal\Sf\Container\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 
 /**
  * A few things don't go as round as we'd expect when enabling the framework
@@ -23,6 +24,16 @@ class FrameworkBundleIntegrationPass implements CompilerPassInterface
                 ->getDefinition('router.default')
                 ->setClass('MakinaCorpus\Drupal\Sf\Routing\Router')
             ;
+        }
+
+        // When NOT in fullstack mode, we need to provide a null implementation
+        // for the controller resolver service, else the container will be
+        // unable to spawn the http kernel service
+        if (!$container->has('controller_resolver')) {
+            $container->addDefinitions([
+                'controller_resolver' => (new Definition())
+                    ->setClass('controller_resolver')
+            ]);
         }
     }
 }
---
+++ src/Http/NullControllerResolver.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace MakinaCorpus\Drupal\Sf\Http;
+
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\HttpFoundation\Request as BaseRequest;
+
+/**
+ * When not working with Symfony full stack, we still need this to be able to
+ * spawn the 'http_kernel' service, even if unused.
+ */
+class NullControllerResolver implements ControllerResolverInterface
+{
+    public function getController(BaseRequest $request)
+    {
+        return false;
+    }
+
+    public function getArguments(BaseRequest $request, $controller)
+    {
+        return [];
+    }
+}
