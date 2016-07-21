3bc0a0ba5819b13adba3444951240c8b069ef718
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-07-21 11:50:35 +0200

    fixes HTTPs detection on broken environment, fixes Drupal is_https global

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\Request as BaseRequest;
  */
 class Request extends BaseRequest
 {
+    private $isSecure;
+
     /**
      * {@inheritDoc}
      */
@@ -26,7 +28,7 @@ class Request extends BaseRequest
             // framework process process, with q= they will fail
             $queryString = $this->server->get('QUERY_STRING');
             $queryString = preg_replace('/q=[^&]*/', '', $queryString);
-            if ($queryString && '&' === $queryString[0]) {
+            if ('&' === $queryString[0]) {
                 if ('&' === $queryString) {
                     $queryString = '';
                 } else {
@@ -42,11 +44,49 @@ class Request extends BaseRequest
         return $requestUri;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getQueryString()
     {
         // Force prepareRequestUri() to be called before query string is fetched
         $this->getRequestUri();
 
         return parent::getQueryString();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function isSecure()
+    {
+        if (null !== $this->isSecure) {
+            return $this->isSecure;
+        }
+        if (parent::isSecure()) {
+            return $this->isSecure = true;
+        }
+
+        // It may happen that some misconfigured environments won't set
+        // the $_SERVER['HTTPS'] variable, we need another way to detect
+        // this, either using Drupal, or port in use.
+        if (443 === (int)$this->server->get('SERVER_PORT', 80)) {
+            return $this->isSecure = true;
+        }
+
+        // Worst than misconfigured, we actually have no idea about what's
+        // what because FPM or any other CGI gives us nothing goog to read,
+        // let's just trust Drupal and pray.
+        if ($GLOBALS['is_https']) {
+            return $this->isSecure = true;
+        }
+
+        // The Drupal base URL might have been hardcoded in settings.php so
+        // let's use that as last resort.
+        if ('https://' === substr($GLOBALS['base_url'], 0, 8)) {
+            return $this->isSecure = true;
+        }
+
+        return $this->isSecure = false;
     }
 }
