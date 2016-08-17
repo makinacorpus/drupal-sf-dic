aa7044b8b4af1e492ae46db2e25641342f2024b6
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-08-17 11:43:56 +0200

    implemented missing time_diff filter in date extension, symfony compatible

--- src/Twig/Extension/DrupalDateExtension.php
+++ src/Twig/Extension/DrupalDateExtension.php
@@ -16,37 +16,73 @@ class DrupalDateExtension extends \Twig_Extension
     {
         return [
             new \Twig_SimpleFilter('date', [$this, 'formatDate']),
+            new \Twig_SimpleFilter('time_diff', [$this, 'formatInterval']),
         ];
+    }
+
+    /**
+     * Because Drupal does need timestamps...
+     *
+     * @param mixed $date
+     *
+     * @return int
+     */
+    private function dateToTimestamp($date)
+    {
+        if (null === $date) {
+            return;
+        }
+        if (is_integer($date)) {
+            return $date;
+        }
+
+        if ($date instanceof \DateTimeInterface) {
+            return $date->getTimestamp();
+        }
+
+        // I don't really agree with this condition but it's necessary
+        // to ensure the compatibility with the default date filter.
+        if (ctype_digit($date) || (!empty($date) && '-' === $date[0] && ctype_digit(substr($date, 1)))) {
+            return (int)$date;
+        }
+
+        $instance = new \DateTime($date);
+        if ($instance) {
+            return $instance->getTimestamp();
+        }
+    }
+
+    public function formatInterval($date, $now = null)
+    {
+        $timestamp = $this->dateToTimestamp($date);
+
+        if (null === $timestamp) {
+            return ''; // I'm really sorry I'm not Mme Irma
+        }
+
+        if (null === $now) {
+            $referenceTimestamp = time();
+        } else {
+            $referenceTimestamp = $this->dateToTimestamp($now);
+
+            if (null === $referenceTimestamp) {
+                return ''; // Still not Mme Irma
+            }
+        }
+
+        return format_interval($referenceTimestamp - $timestamp);
     }
 
     public function formatDate($date, $format = 'medium', $timezone = null, $langcode = null)
     {
-        $timestamp = 0;
+        $timestamp = $this->dateToTimestamp($date);
 
-        if (is_integer($date)) {
-            $timestamp = $date;
-        }
-        elseif ($date instanceof \DateTimeInterface) {
-            $timestamp = $date->getTimestamp();
-        }
-        elseif (is_string($date)) {
-            // I don't really agree with this condition but it's necessary
-            // to ensure the compatibility with the default date filter.
-            if (ctype_digit($date) || (!empty($date) && '-' === $date[0] && ctype_digit(substr($date, 1)))) {
-                $timestamp = (integer) $date;
-            } else {
-                $timestamp = strtotime($date);
-                if ($timestamp === false) {
-                    return '';
-                }
-            }
-        }
-        else {
-          return '';
+        if (null === $timestamp) {
+            return ''; // I'm really sorry I'm not Mme Irma
         }
 
         $drupalDateTypes = system_get_date_types();
-        
+
         if (isset($drupalDateTypes[$format])) {
             $formatted = format_date($timestamp, $format, '', $timezone, $langcode);
         } else {
