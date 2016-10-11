05c2bd2dabcc68db2b4c78858190329649ea61f8
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-10-11 13:04:12 +0200

    added render_block() and node_field() twig functions

---
+++ src/Twig/Extension/DrupalBlockExtension.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace MakinaCorpus\Drupal\Sf\Twig\Extension;
+
+/**
+ * Renders Drupal blocks into Twig templates
+ */
+class DrupalBlockExtension extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction('render_block', [$this, 'doRenderBlock'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    /**
+     * Render a Drupal block
+     */
+    public function doRenderBlock($module, $delta)
+    {
+        // $block = block_load($module, $delta);
+        $blocks = module_invoke($module, 'block_info');
+
+        if (!isset($blocks[$delta])) {
+            return;
+        }
+
+        $block = (object)$blocks[$delta];
+        $block->module = $module;
+        $block->delta = $delta;
+        $block->status = 1;
+        $block->region = 'content';
+        $block->weight = 0;
+        $block->theme = $GLOBALS['theme_key'];
+
+        $blockList = [$block];
+        drupal_alter('block_list', $blockList);
+
+        $render_array = _block_get_renderable_array(_block_render_blocks($blockList));
+
+        return drupal_render($render_array);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'drupal_block';
+    }
+}
--- src/Twig/Extension/DrupalNodeExtension.php
+++ src/Twig/Extension/DrupalNodeExtension.php
@@ -2,6 +2,8 @@
 
 namespace MakinaCorpus\Drupal\Sf\Twig\Extension;
 
+use Drupal\node\NodeInterface;
+
 /**
  * Drupal image style display
  */
@@ -14,7 +16,33 @@ class DrupalNodeExtension extends \Twig_Extension
     {
         return [
             new \Twig_SimpleFunction('node_view', [$this, 'nodeView'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFunction('node_field', [$this, 'fieldView'], ['is_safe' => ['html']]),
         ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters()
+    {
+        return [
+
+        ];
+    }
+
+    public function fieldView($node, $field, $view_mode = 'default')
+    {
+        if (!$node instanceof NodeInterface) {
+            return '';
+        }
+
+        if (!field_get_items('node', $node, $field)) {
+            return '';
+        }
+
+        $output = field_view_field('node', $node, $field, $view_mode);
+
+        return drupal_render($output);
     }
 
     public function nodeView($node, $view_mode = 'full')
