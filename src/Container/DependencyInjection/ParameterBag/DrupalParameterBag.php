5c11e8473aad64c3a0dd526a8c46001359b83174
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-11-09 14:35:23 +0100

    fixes problems with kernel.cache_dir value when rebuilding the kernel

--- src/Container/DependencyInjection/ParameterBag/DrupalParameterBag.php
+++ src/Container/DependencyInjection/ParameterBag/DrupalParameterBag.php
@@ -11,6 +11,33 @@ class DrupalParameterBag extends ParameterBag
      */
     public function get($name)
     {
+        if ('kernel.' === substr($name, 0, 7)) {
+            // Kernel variables must be driven by the kernel only: here is the
+            // explainaintion (not a simple one):
+            //  - at kernel boot time, we compute a kernel.cache_dir using
+            //    the defined global $conf variable, and suffixing with the
+            //    env name;
+            //  - we leave the global $conf untouched, because if we do need to
+            //    rebuild the kernel and container from the same request, it
+            //    must keep the original value to append once again the env
+            //    name;
+            //  - some compiler passes or other pieces of code will directly
+            //    fetch the kernel parameter to resolve values (for example:
+            //    "%kernel.cache_dir%/annotations" for the annotations cached
+            //    reader directory);
+            //  - this will use the $conf value, which has not the environment
+            //    name prepended, and won't look for the right file at the right
+            //    place.
+            if (parent::has($name)) {
+                return parent::get($name);
+            }
+            if (isset($GLOBALS['conf']) && array_key_exists($name, $GLOBALS['conf'])) {
+                return $GLOBALS['conf'][$name];
+            }
+
+            throw new \InvalidArgumentException(sprintf("%s: parameter does not exist"));
+        }
+
         if (isset($GLOBALS['conf']) && array_key_exists($name, $GLOBALS['conf'])) {
             return $GLOBALS['conf'][$name];
         }
