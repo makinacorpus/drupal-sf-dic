ba1b0ea3deadcb023e7ec2ac869bea2a49255c32
Sebastien Corbin <seb.corbin@gmail.com>
2016-11-16 11:26:31 +0100

    Add file_icon() to twig extension

--- src/Twig/Extension/DrupalFileExtension.php
+++ src/Twig/Extension/DrupalFileExtension.php
@@ -14,6 +14,7 @@ class DrupalFileExtension extends \Twig_Extension
     {
         return [
             new \Twig_SimpleFunction('file_url', [$this, 'createFileUrl'], ['is_safe' => ['html']]),
+            new \Twig_SimpleFunction('file_icon', [$this, 'fileIcon'], ['is_safe' => ['html']]),
             new \Twig_SimpleFunction('human_size', [$this, 'humanFileSize'], ['is_safe' => ['html']]),
         ];
     }
@@ -29,6 +30,17 @@ class DrupalFileExtension extends \Twig_Extension
         $factor = floor((strlen($size) - 1) / 3);
 
         return sprintf("%.{$decimals}f", $size / pow(1024, $factor)).@$sz[(int)$factor - 1]."o";
+    }
+
+    public function fileIcon($file)
+    {
+        $file = (object) $file;
+        $build = [
+            '#theme' => 'file_icon',
+            '#file'  => $file,
+        ];
+
+        return drupal_render($build);
     }
 
     /**
