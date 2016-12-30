6beeff8ca46cf9e3e5afb16c47a94c3d2ba20e97
Pierre Rineau <pierre.rineau@makina-corpus.com>
2016-12-30 14:37:16 +0100

    performance: deactivate user_node_load() and reproduce its behaviour in \Drupal\node\CompatNodeController

--- lib/Drupal/node/CompatNodeController.php
+++ lib/Drupal/node/CompatNodeController.php
@@ -9,7 +9,15 @@ class CompatNodeController extends \NodeController
      */
     protected function buildQuery($ids, $conditions = [], $revision_id = false)
     {
-        return parent::buildQuery($ids, $conditions, $revision_id)
+        $query = parent::buildQuery($ids, $conditions, $revision_id);
+
+        // user_node_load() goes here, much less requests in the end.
+        $query->leftJoin('users', 'u', 'u.uid = base.uid');
+        $query->addField('u', 'name', 'name');
+        $query->addField('u', 'picture', 'picture');
+        $query->addField('u', 'data', 'data');
+
+        return $query
             ->extend('\Drupal\Core\Entity\FetchClassQueryExtender')
             ->setObjectClass('\Drupal\node\Node')
         ;
