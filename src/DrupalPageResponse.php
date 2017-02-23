a9762f1a31c97ce04babc11cf2154559e4338d37
Pierre Rineau <pierre.rineau@makina-corpus.com>
2017-02-23 19:46:00 +0100

    webprofiler: we cannot use drupal_page_footer() because it triggers flush() calls

--- src/DrupalPageResponse.php
+++ src/DrupalPageResponse.php
@@ -29,7 +29,18 @@ class DrupalPageResponse extends Response
     {
         parent::send();
 
-        drupal_page_footer();
+        // We actually need to rewrite the drupal_page_footer() function
+        // manually because we can't let it call the flush() method, everything
+        // has already been flushed and it will trigger PHP notices.
+        module_invoke_all('exit');
+        drupal_session_commit();
+        if (variable_get('cache', 0) && ($cache = drupal_page_set_cache())) {
+          drupal_serve_page_from_cache($cache);
+        }
+        _registry_check_code(REGISTRY_WRITE_LOOKUP_CACHE);
+        drupal_cache_system_paths();
+        module_implements_write_cache();
+        system_run_automated_cron();
 
         return $this;
     }
