873885fefb1d085ec0864fd4cae6d8cf9dc7e0f5
Pierre Rineau <pierre.rineau@makina-corpus.com>
2017-02-28 15:34:04 +0100

    weirdest thing that ever happened: current_path() was evaluated...

--- lib/Drupal/Core/Path/AliasManager.php
+++ lib/Drupal/Core/Path/AliasManager.php
@@ -92,7 +92,11 @@ class AliasManager implements AliasManagerInterface, CacheDecoratorInterface
             // If this service is being waked up during Symfony cache warming
             // the 'current_path' function does not already exists, avoid WSOD
             // in these conditions.
-            $this->cacheKey = 'sf:' . function_exists('current_path') ? current_path() : $_GET['q'];
+            if (function_exists('current_path')) {
+                $this->cacheKey = 'sf:' .  current_path();
+            } else {
+                $this->cacheKey = 'sf:' . ($_GET['q'] ?: '<front>' );
+            }
         }
 
         $this->data += [self::ALIAS => [], self::SOURCE => []];
