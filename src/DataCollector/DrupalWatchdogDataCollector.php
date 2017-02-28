8def699fa98ed42560a1fd087727fe0c1cd26be8
Pierre Rineau <pierre.rineau@makina-corpus.com>
2017-02-28 16:50:02 +0100

    webprofiler: basics for watchdog entries logging

---
+++ src/DataCollector/DrupalWatchdogDataCollector.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace MakinaCorpus\Drupal\Sf\DataCollector;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+use Symfony\Component\HttpKernel\DataCollector\LateDataCollectorInterface;
+
+/**
+ * Collects watchdog entries.
+ *
+ * @see sf_dic_watchdog()
+ */
+class DrupalWatchdogDataCollector extends DataCollector implements LateDataCollectorInterface
+{
+    /**
+     * Default constructor
+     */
+    public function __construct()
+    {
+        if (empty($this->data)) {
+            $this->data = [];
+        }
+    }
+
+    /**
+     * Log new watchdog entry
+     */
+    public function logEntry(array $logEntry)
+    {
+        $this->data[] = $logEntry;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function collect(Request $request, Response $response, \Exception $exception = null)
+    {
+        return $this->data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function lateCollect()
+    {
+        return $this->data;
+    }
+
+    /**
+     * Get data
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'drupal_watchdog';
+    }
+}
