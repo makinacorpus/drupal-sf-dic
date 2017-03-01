e638fa289bd2bf194c2565163a83bbe4de66c480
Pierre Rineau <pierre.rineau@makina-corpus.com>
2017-03-01 19:46:26 +0100

    avoid PHP warnings due to Drupal page caching in DrupalPageResponse::send()

--- src/DrupalPageResponse.php
+++ src/DrupalPageResponse.php
@@ -29,7 +29,11 @@ class DrupalPageResponse extends Response
      */
     public function send()
     {
-        parent::send();
+        // We cannot directly call the parent implementation, because if Drupal
+        // caches the result and sends it with drupal_serve_page_from_cache()
+        // it will send headers in between.
+        $this->sendHeaders();
+        $this->sendContent();
 
         // We actually need to rewrite the drupal_page_footer() function
         // manually because we can't let it call the flush() method, everything
@@ -39,6 +43,17 @@ class DrupalPageResponse extends Response
         if (variable_get('cache', 0) && ($cache = drupal_page_set_cache())) {
           drupal_serve_page_from_cache($cache);
         }
+
+        // And here it is for the output buffer flush, this means that
+        // everything that happens during hook_exit() invokation and session
+        // commit will happen before we loose the thread, sad Drupal is sad
+        // but we have no other choice.
+        if (function_exists('fastcgi_finish_request')) {
+            fastcgi_finish_request();
+        } elseif ('cli' !== PHP_SAPI) {
+            static::closeOutputBuffers(0, true);
+        }
+
         _registry_check_code(REGISTRY_WRITE_LOOKUP_CACHE);
         drupal_cache_system_paths();
         module_implements_write_cache();
