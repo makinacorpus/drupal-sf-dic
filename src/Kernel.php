bbae1cf1cb614960a9623f6e368fff5bc0eedd47
Pierre Rineau <pierre.rineau@makina-corpus.com>
2018-02-05 18:02:06 +0100

    do not realpath directories when not necessary: forgotten occurence of realpath()

--- src/Kernel.php
+++ src/Kernel.php
@@ -81,7 +81,7 @@ abstract class Kernel extends BaseKernel
             $logsDirFromConf = true;
         }
 
-        if (!$logsDirFromConf && ($logDir = realpath($this->logDir))) {
+        if (!$logsDirFromConf) {
             $this->logDir = $this->ensureDirectory($this->logDir);
         }
 
